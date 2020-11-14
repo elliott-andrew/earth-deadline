639b311c5e27e1e0480caad87c7efc83f8b4f7bf
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-14 15:52:35 -0500

    Updated countdown function with w3 schools and added milliseconds

--- public/javascripts/countdown15.js
+++ public/javascripts/countdown15.js
@@ -1,16 +1,32 @@
-const today = new Date()
+// Set the date we're counting down to
+const countDownDate = new Date("Nov 25, 2032 09:00:00").getTime();
 
-const endDate = new Date('November 25, 2032')
-const milliEachDay = 24 * 60 * 60 * 1000
-const timeLeft = (endDate.getTime() - today.getTime())
-const eDaysLeft = timeLeft / milliEachDay
-let daysLeft = Math.floor(eDaysLeft)
-let yearsLeft = 0
-if (daysLeft > 365) {
-  yearsLeft = Math.floor(daysLeft / 366)
-  daysLeft = daysLeft % 366
-}
-const eHrsLeft = (eDaysLeft - daysLeft) * 24
-const hrsLeft = Math.floor(eHrsLeft)
-const minsLeft = Math.floor((eHrsLeft - hrsLeft) * 60)
-console.log(yearsLeft + ' years ' + daysLeft + ' days ' + hrsLeft + ' hours ' + minsLeft + ' minutes')
+// Update the count down every 1 second
+const x = setInterval(function() {
+
+  // Get today's date and time
+  const now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  if (days > 365) {
+  const yearsLeft = Math.floor(days / 365)
+  days = days % 365
+  }
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const milliseconds = Math.floor(distance % 1000);  
+  // Output the result in an element with id="demo"
+  console.log(days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s " + milliseconds + "m");
+    
+  // If the count down is over, write some text 
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "EXPIRED";
+//   }
+}, 1000);
