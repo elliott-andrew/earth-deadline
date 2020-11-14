1dd925e18aa1f35500d965c91ed7c9d533fbc16d
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-14 13:17:50 -0500

    Added handlebars requirements to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var exphbs = require('express-handlebars');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,10 +9,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
 app.use(express.json());
