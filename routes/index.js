350b40133bb0e33e46897d618bc452418f232a23
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-15 21:25:42 -0500

    Updated navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Earth Countdown' })
+  res.render('index', { title: 'Earth Deadline' })
 })
 
 module.exports = router
